5e2f6a8d353c9f093e2cff015b4e2a234c936493
Elwe-Thingol <benj_s91@hotmail.com>
2017-03-14 19:17:35 +0100

    Revert "badly comment correction"
    This reverts commit 68c75b2259eccad6bf1fa649aad16f6b5ce34c8b.

--- 226_Git1/src/ch/csbe/git/HelloWorld.java
+++ 226_Git1/src/ch/csbe/git/HelloWorld.java
@@ -1,12 +1,7 @@
 package ch.csbe.git;
 
 public class HelloWorld {
-	
-/**
- * 
- * blablabla
- * @param args
- */
+
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
